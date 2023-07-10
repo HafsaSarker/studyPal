1a65ec64b5b3c8f8887a2857e484a77d9c64da29
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-07-10 15:56:56 -0400

    modify  registration and login ui

--- studyPal/src/pages/Auth/login/Login.jsx
+++ studyPal/src/pages/Auth/login/Login.jsx
@@ -1,17 +1,43 @@
+import { useState } from 'react'
 import '../AuthModal.css'
 
 function Login() {
+    const [formData, setFormData] = useState({
+        email:'',
+        password: '',
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const loginUser = async(e) =>{
+        e.preventDefault()
+    }
   return (
     <div>
-        <form className='logIn-form'>
+        <form className='logIn-form' onSubmit={loginUser}>
             <h2>Welcome back!</h2>
             <label>
-                EMAIL
-                <input type="email" />
-            </label>
+                NAME 
+                    <input 
+                        type="text"
+                        name='name'
+                        onChange={handleChange}
+                    />
+                </label>
             <label>
                 PASSWORD
-                <input type="password" />
+                <input 
+                    type="password"
+                    name='password'
+                    onChange={handleChange}
+                />
             </label>
             
             <button className='primary-button'>Log in</button>
--- studyPal/src/pages/Auth/register/Register.jsx
+++ studyPal/src/pages/Auth/register/Register.jsx
@@ -1,31 +1,68 @@
+import { useState } from 'react'
 import '../AuthModal.css'
 
 function Register() {
-  return (
-    <div>
-        <form className="signUp-form">
-            <h2>Create an account</h2>
-            <label>
-            NAME 
-            <input type="text" />
-            </label>
-            <label>
-                EMAIL
-                <input type="email" />
-            </label>
-            <label>
-                PASSWORD
-                <input type="password" />
-            </label>
-            <label>
-                CONFIRM PASSWORD
-                <input type="password" />
-            </label>
-            
-            <button className='primary-button'>Sign up</button>
-        </form>
-    </div>
-  )
+    const [formData, setFormData] = useState({
+        name: '',
+        email:'',
+        password: '',
+        confirmPass: ''
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const createAccount = async(e) =>{
+        e.preventDefault()
+    }
+
+    return (
+        <div>
+            <form className="signUp-form" onSubmit={createAccount}>
+                <h2>Create an account</h2>
+                <label>
+                NAME 
+                    <input 
+                        type="text"
+                        name='name'
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    EMAIL
+                    <input 
+                        type="email"
+                        name='email'
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    PASSWORD
+                    <input 
+                        type="password"
+                        name='password'
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    CONFIRM PASSWORD
+                    <input 
+                        type="password"
+                        name='confirmPass'
+                        onChange={handleChange}
+                    />
+                </label>
+                
+                <button className='primary-button'>Sign up</button>
+            </form>
+        </div>
+    )
 }
 
 export default Register
