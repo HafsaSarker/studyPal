a3bfe134982803d4dc3f1c5e04b8b8e36cf883a6
HafsaSarker <hafsahaqs@gmail.com>
2023-09-29 15:52:41 -0400

    transition dashboard css -> tailwind

--- client/src/Components/sideNav/SideNav.jsx
+++ client/src/Components/sideNav/SideNav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function SideNav() {
   return (
     <nav className="flex h-96 flex-col items-center justify-start rounded-r-xl border-r-2 border-r-light-blue px-5 py-12 text-center text-white">
-      <ul>
+      <ul className=" w-32">
         <Link to="/dashboard">
           <li className="my-4 cursor-pointer rounded-full bg-light-blue px-4 py-1 transition duration-300 hover:bg-hover-blue">
             Dashboard
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,6 @@ import SideNav from "../../Components/sideNav/SideNav";
 import axios from "axios";
 import { path } from "../../API_PATH";
 import Card from "../../Components/Card/Card";
-import "./Dashboard.css";
 import { toast } from "react-toastify";
 import { useEffect, useState } from "react";
 import { resetNotif } from "../../features/notification/notifSlice";
@@ -11,133 +10,124 @@ import { useDispatch, useSelector } from "react-redux";
 import Spinner from "../../Components/spinner/Spinner";
 
 export default function Dashboard() {
-  // const navigate = useNavigate()
-  // const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  // const { user } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.auth);
 
-  // const [studySets, setStudySets] = useState(null)
+  const [studySets, setStudySets] = useState(null);
 
-  // const { searchInput } = useSelector((state) => state.search)
+  const { searchInput } = useSelector((state) => state.search);
 
-  // //for sending alerts/messages to user
-  // const isEditState = useSelector((state) => state.notif.isEdited)
-  // const isDeleteState = useSelector((state) => state.notif.isDeleted)
-  // const isCreateState = useSelector((state) => state.notif.isCreated)
+  //for sending alerts/messages to user
+  const isEditState = useSelector((state) => state.notif.isEdited);
+  const isDeleteState = useSelector((state) => state.notif.isDeleted);
+  const isCreateState = useSelector((state) => state.notif.isCreated);
 
-  // useEffect(() => {
-  //     const fetchAllSets = async() => {
-  //         const config = {
-  //             headers: {
-  //               Authorization: `Bearer ${user.token}`,
-  //             },
-  //         }
-  //         try {
-  //             const {data} = await axios.get(path, config)
-  //             setStudySets(data.allSets)
-  //         } catch (error) {
-  //             console.log(error);
-  //         }
-  //     }
+  useEffect(() => {
+    const fetchAllSets = async () => {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      try {
+        const { data } = await axios.get(path, config);
+        setStudySets(data.allSets);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  //     if(!user){
-  //         navigate('/')
-  //     }else {
-  //         fetchAllSets()
-  //     }
-  //     fetchAllSets()
-  // },[user, navigate])
+    if (!user) {
+      navigate("/");
+    } else {
+      fetchAllSets();
+    }
+    fetchAllSets();
+  }, [user, navigate]);
 
-  // useEffect(() => {
-  //     const sendNotif = () => {
-  //         let message = ''
+  useEffect(() => {
+    const sendNotif = () => {
+      let message = "";
 
-  //         if(isEditState){
-  //             message = 'successfully updated'
-  //         }
-  //         else if(isDeleteState){
-  //             message = 'successfully deleted'
-  //         }
-  //         else if(isCreateState){
-  //             message = 'successfully created'
-  //         }
-  //         else{
-  //             message = null
-  //         }
-  //         message ? toast.success(message) : null
-  //     }
-  //     sendNotif()
-  //     dispatch(resetNotif())
-  // },[isEditState, isDeleteState, isCreateState, resetNotif])
+      if (isEditState) {
+        message = "successfully updated";
+      } else if (isDeleteState) {
+        message = "successfully deleted";
+      } else if (isCreateState) {
+        message = "successfully created";
+      } else {
+        message = null;
+      }
+      message ? toast.success(message) : null;
+    };
+    sendNotif();
+    dispatch(resetNotif());
+  }, [isEditState, isDeleteState, isCreateState, resetNotif]);
 
-  // const filteredSets = (studySets && searchInput) && studySets.filter((item) => item.setTitle.toLowerCase().includes(searchInput.toLowerCase()))
+  const filteredSets =
+    studySets &&
+    searchInput &&
+    studySets.filter((item) =>
+      item.setTitle.toLowerCase().includes(searchInput.toLowerCase()),
+    );
 
   return (
-    <div className="dashboard">
-      <div className="side-nav">
+    <div className="flex items-start justify-start text-center text-dark-green">
+      <div className="mt-24">
         <SideNav />
       </div>
 
-      <h2>dashboard</h2>
+      <div className="mx-20 my-12 flex w-full flex-col items-center justify-center">
+        {!studySets && (
+          <div className="mt-24">
+            <Spinner />
+          </div>
+        )}
 
-      {/* <div className="dashboard-main">
-                {
-                    !studySets && 
-                    <div className="spinner">
-                    <Spinner />
-                    </div>
-                }
+        {studySets && studySets.length > 0 && (
+          <>
+            <h2 className="mb-12 mt-5">click on a set to start studying!</h2>
 
-                {(studySets && studySets.length > 0) &&
-                (   
-                    <>
-                        <h2 className='dashbordTitle'>click on a set to start studying!</h2>
-                        
-                        <div className="populated-dashboard">
-                        {
-                            !searchInput? (
-                            studySets.map((item, index) =>
-                            <Card 
-                                key={index}
-                                setTitle={item.setTitle}
-                                img={item.img}
-                                createdAt={item.createdAt}
-                                id={item._id}
-                            />
-                             
-                        )
-                        ):(
-                            filteredSets.length < 1 ? (
-                                <p>No result</p>
-                            ):(
-                                filteredSets.map((item, index) =>
-                                    <Card 
-                                        key={index}
-                                        setTitle={item.setTitle}
-                                        img={item.img}
-                                        createdAt={item.createdAt}
-                                        id={item._id}
-                                    />
-                                )
-                            )
-                        )}                  
-                        </div>  
-                    </>
-                         
-                )}
+            <div className=" flex flex-row flex-wrap items-start justify-center gap-12">
+              {!searchInput ? (
+                studySets.map((item, index) => (
+                  <Card
+                    key={index}
+                    setTitle={item.setTitle}
+                    img={item.img}
+                    createdAt={item.createdAt}
+                    id={item._id}
+                  />
+                ))
+              ) : filteredSets.length < 1 ? (
+                <p>No result</p>
+              ) : (
+                filteredSets.map((item, index) => (
+                  <Card
+                    key={index}
+                    setTitle={item.setTitle}
+                    img={item.img}
+                    createdAt={item.createdAt}
+                    id={item._id}
+                  />
+                ))
+              )}
+            </div>
+          </>
+        )}
 
-                {
-                    (studySets && studySets.length < 1) &&
-                    <div className="empty-dashboard">
-                        <img src='./feedbackCat.png' />
-                        <p className='primary-p'>oh shucks...you currently have no sets</p>
-                        <Link to='/createSet'>
-                            <button className='secondary-button'>Create one here</button>
-                        </Link>
-                    </div>
-                }
-                
-            </div> */}
+        {studySets && studySets.length < 1 && (
+          <div className="mt-24 flex flex-col items-center justify-center">
+            <img className="h-14 w-14" src="./feedbackCat.png" />
+            <p className="primary-p">oh shucks...you currently have no sets</p>
+            <Link to="/createSet">
+              <button className="secondary-button">Create one here</button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
